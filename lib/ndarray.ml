e63747bee1275e547f0f3b0bf9d16bf754a537f8
lukstafi <lukstafi@gmail.com>
2022-12-03 16:02:07 +0100

    Fusion by source transformation

--- lib/ndarray.ml
+++ lib/ndarray.ml
@@ -1,8 +1,15 @@
 (** Multidimensional arrays of float32. *)
 (* The code is inspired by OWL - OCaml Scientific Computing:
  * Copyright (c) 2016-2022 Liang Wang <liang@ocaml.xyz> *)
-module A = Bigarray
-type elt = A.float32_elt
-type t = (float, elt, c_layout) A.Genarray.t
+module A = Bigarray.Genarray
+type elt = Bigarray.float32_elt
+type t = (float, elt, Bigarray.c_layout) A.t
 
+ let dims: t -> 'a = A.dims
+ let create = A.create Bigarray.Float32 Bigarray.C_layout
+ let empty = create [||]
  
+let create_ones =
+  let v = A.create Bigarray.Float32 Bigarray.C_layout in
+  (* TODO: FIXME: NOT IMPLEMENTED *)
+  v
