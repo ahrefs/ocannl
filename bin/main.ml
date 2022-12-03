501ab5179f792329a54aaa4bbc3997dde52b78ee
lukstafi <lukstafi@gmail.com>
2022-12-03 16:02:28 +0100

    Cleanup of Dune dependencies

--- bin/main.ml
+++ bin/main.ml
@@ -1,71 +1,9 @@
 open Base
-(* open Matplotlib *)
-module L = Lacaml.S
 
-module F = Caml.Format
-module LF = Lacaml.Io
+open Matplotlib
 
-let () =
-  let m = 5 in
-  let n = 3 in
+module F = Ocannl.Formula
 
-  let data_mat = L.Mat.create m n in
-  let data_mat_copy = L.Mat.create m n in
-  let res_len = max 1 (max m n) in
-  let res_mat = L.Mat.create_mvec res_len in
-  let res_mat_copy = L.Mat.create_mvec res_len in
-
-  for i = 1 to m do
-    let v_ref = ref 0.0 in
-
-    for j = 1 to n do
-      let randf = Random.float 200.0 -. 100.0 in
-      v_ref := !v_ref +. Float.of_int j *. randf;
-      data_mat.{i, j} <- randf;
-      data_mat_copy.{i, j} <- randf;
-    done;
-
-    let v = !v_ref in
-    res_mat.{i, 1} <- v;
-    res_mat_copy.{i, 1} <- v;
-  done;
-
-  F.printf
-    "\
-      @[<2>Predictor variables:\n\
-        @\n\
-        %a@]\n\
-      @\n\
-      @[<2>Response variable:\n\
-        @\n\
-        %a@]@\n\
-      @\n"
-    LF.pp_fmat data_mat
-    LF.pp_rfvec (L.Mat.col res_mat 1);
-
-  let rank = L.gelsd data_mat res_mat in
-
-  F.printf
-    "\
-      @[<2>Regression weights:\n\
-        @\n\
-        %a@]\n\
-      @\n\
-      Rank: %d@\n@\n"
-    LF.pp_rfvec (L.Mat.col res_mat 1)
-    rank;
-
-  let y = L.gemv data_mat_copy (L.Mat.col res_mat 1) in
-  let b = L.Mat.col res_mat_copy 1 in
-
-  F.printf
-    "\
-      @[<2>Check result (must be close to 0):\n\
-        @\n\
-        %a@]@\n"
-    LF.pp_rfvec (L.Vec.sub y b)
-
-(* 
 (* Copy of https://github.com/LaurentMazare/ocaml-matplotlib/blob/master/examples/pyplot.ml *)
 let () =
   let xs = List.init 120 ~f:Float.of_int in
@@ -88,4 +26,3 @@ let () =
   (* Mpl.set_backend (Mpl.Backend.Other "Qt5Agg"); *)
   Mpl.set_backend (Mpl.Backend.Default);
   Mpl.show ()
-   *)
