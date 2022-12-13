276310523113ca8641efcbc0572957f2f5ff033b
lukstafi <lukstafi@gmail.com>
2022-12-13 18:24:04 +0100

    Switch from matplotlib to ocaml-canvas Simple plotting will be part of OCaNNL.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Inspired by Andrej Karpathy's [Micrograd](https://github.com/karpathy/micrograd)
 
 MetaOCaml's generated code can be both run directly (encapsulating the steps: natively compile, dynlink and invoke), or it can be offshored, saved as text, etc. It offers debuggability and lots of flexibility going forward.
 
-OCANNL's codeflow is as follows:
+OCaNNL's codeflow is as follows:
 1. Construct a model, represented by a loss formula composed of appropriate subformulas all the way down to parameters. Inputs are just parameters that the training loop treats specially. The primitives are in the module [`Formula`](lib/formula.ml). The subformulas store code pieces that are not compiled (not `Runcode.run`ed) yet.
 2. MetaOCaml's `Runcode.run` combines compiling and executing the code. Executing code of a `Formula` specializes it to particular [`Ndarray`](lib/ndarray.ml) dimensions, and outputs the procedures for forward inference and backward gradient propagation.
 3. A step of the training loop sets the inputs, invokes the "forward" and "backprop" procedures, and updates the parameters (adding to a parameter's `value`, its `grad` scaled by the learning rate). Gradient zeroing is performed automatically by the "backprop" procedure.
@@ -25,11 +25,6 @@ Some ideas regarding installation (skip or substitute equivalent actions etc.):
 * opam remote add metaocaml git+https://github.com/metaocaml/metaocaml-opam.git
 * opam install dune
 * eval $(opam env)
-* pip install matplotlib
-* opam install matplotlib
-* sudo apt-get install qt5-assistant
-* pip install PyQt5
-* sudo apt-get install pyplot*
-* opam install pyplot
+* opam install ocaml-canvas
 * cd ~/ocannl
 * dune exec ocannl
--- bin/main.ml
+++ bin/main.ml
@@ -1,28 +1,73 @@
 open Base
-
-open Matplotlib
-
 module F = Ocannl.Formula
+open OcamlCanvas.V1
 
-(* Copy of https://github.com/LaurentMazare/ocaml-matplotlib/blob/master/examples/pyplot.ml *)
-let () =
-  let xs = List.init 120 ~f:Float.of_int in
-  let ys1 = List.map xs ~f:(fun i -> Float.sin (i /. 20.)) in
-  let ys2 = List.map xs ~f:(fun i -> Float.cos (i /. 12.)) in
-  let xs = Array.of_list xs in
-  let ys1 = Array.of_list ys1 in
-  let ys2 = Array.of_list ys2 in
-  Pyplot.xlabel "x";
-  Pyplot.ylabel "y";
-  Pyplot.grid true;
-  Pyplot.plot ~color:Red ~xs ys1;
-  Pyplot.plot ~color:Green ~linestyle:Dotted ~linewidth:2. ~xs ys2;
-  Pyplot.fill_between ~alpha:0.3
-    xs ys1 (Array.create ~len:(Array.length ys1) 0.);
-  Pyplot.legend ~labels:[|"$y=\\sin(x/20)$"; "$y=\\cos(x/12)$"|] ();
-  Mpl.savefig "test.png";
-  let data = Mpl.plot_data `png in
-  Stdio.Out_channel.write_all "test2.png" ~data;
-  (* Mpl.set_backend (Mpl.Backend.Other "Qt5Agg"); *)
-  Mpl.set_backend (Mpl.Backend.Default);
-  Mpl.show ()
+let _ =
+
+  Backend.init ();
+
+  let c = Canvas.createOnscreen ~title:"Hello world" ~pos:(300, 200) ~size:(300, 200) () in
+
+  Canvas.setFillColor c Color.orange;
+  Canvas.fillRect c ~pos:(0.0, 0.0) ~size:(300.0, 200.0);
+
+  Canvas.setStrokeColor c Color.cyan;
+  Canvas.setLineWidth c 10.0;
+  Canvas.clearPath c;
+  Canvas.moveTo c (5.0, 5.0);
+  Canvas.lineTo c (295.0, 5.0);
+  Canvas.lineTo c (295.0, 195.0);
+  Canvas.lineTo c (5.0, 195.0);
+  Canvas.closePath c;
+  Canvas.stroke c;
+
+  Canvas.setFont c "Liberation Sans" ~size:36.0
+    ~slant:Font.Roman ~weight:Font.bold;
+
+  Canvas.setFillColor c (Color.of_rgb 0 64 255);
+  Canvas.setLineWidth c 1.0;
+  Canvas.save c;
+  Canvas.translate c (150.0, 100.0);
+  Canvas.rotate c (-. Const.pi_8);
+  Canvas.fillText c "Hello world !" (-130.0, 20.0);
+  Canvas.restore c;
+
+  Canvas.show c;
+
+  let e1 =
+    React.E.map (fun { Event.canvas = _; timestamp = _; data = () } ->
+        Backend.stop ()
+      ) Event.close
+  in
+
+  let e2 =
+    React.E.map (
+      function { Event.canvas = _; timestamp = _;
+                 data = { Event.key = key; char = _; flags = _ }; _ } ->
+        if phys_equal key KeyEscape then
+          Backend.stop ()
+      ) Event.key_down
+  in
+
+  let e3 =
+    React.E.map (fun { Event.canvas = _; timestamp = _;
+                       data = { Event.position = (x, y); button = _ } } ->
+        Canvas.setFillColor c Color.red;
+        Canvas.clearPath c;
+        Canvas.arc c ~center:(Float.of_int x, Float.of_int y)
+          ~radius:5.0 ~theta1:0.0 ~theta2:Float.(2.0 * Const.pi) ~ccw:false;
+        Canvas.fill c ~nonzero:false
+      ) Event.button_down
+  in
+
+  let frames = ref Int64.zero in
+
+  let e4 =
+    React.E.map (fun { Event.canvas = _; timestamp = _; data = _ } ->
+        frames := Int64.(!frames + one)
+      ) Event.frame
+  in
+
+  Backend.run (fun () ->
+      ignore e1; ignore e2; ignore e3; ignore e4;
+      Caml.Printf.printf "Displayed %Ld frames. Goodbye !\n" !frames)
