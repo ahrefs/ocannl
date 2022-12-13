1dc03c92f1f4d1cc3cd46752a6d989b4da6d6626
lukstafi <lukstafi@gmail.com>
2022-12-13 10:25:46 +0100

    Code generation cleanup: no let dims, no () stmts

--- lib/formula.ml
+++ lib/formula.ml
@@ -1,18 +1,19 @@
 open Base
 
+(** Uses [code option], i.e. [None] instead of [.< () >.], to improve readability of generated code. *)
 type submodel = {
   toplevel_forward: (unit -> unit) Codelib.code;
   (** Only apply at the root, since otherwise some computation may be elided (incorrect results). *)
   toplevel_backprop: (unit -> unit) Codelib.code;
   (** Only apply at the root! Gradients propagate from the top and are only propagated once. Zeroes
       the gradients before propagating. *)
-  forward_body: unit Codelib.code;
+  forward_body: unit Codelib.code option;
   init_values: unit Codelib.code;
   (** Initializes the values. Computed only once per model compilation. *)
   init_grads: unit Codelib.code;
   (** Initializes the gradient data: typically, simply creates the ndarrays.
       Gradients are zeroed separately. *)
-  backprop_body: unit Codelib.code;
+  backprop_body: unit Codelib.code option;
   zero_grads: unit Codelib.code;
   (** Initializes the backpropagation phase. Computed once per backpropagation. *)
   node_id: int;
@@ -48,15 +49,17 @@ let binop ~label ~name ~op_body ~grad_body m1 m2 =
   let op_body = op_body ~nv ~n1v ~n2v in
   (* The code needs to be included in the order it was computed! *)
   let forward_body =
-    if m1.processed && m2.processed then op_body
-    else if m1.processed then (.< .~(m2.forward_body); .~op_body >.)
-    else if m2.processed then (.< .~(m1.forward_body); .~op_body >.)
-    else if l2r_comp_order then (.< .~(m1.forward_body); .~(m2.forward_body); .~op_body >.)
-    else (.< .~(m2.forward_body); .~(m1.forward_body); .~op_body >.) in
+    match m1.processed, m1.forward_body, m2.processed, m2.forward_body with
+    | true, _, true, _ | true, _, _, None | _, None, true, _ | _, None, _, None -> op_body
+    | false, Some m1_body, false, Some m2_body when l2r_comp_order ->
+      (.< .~m1_body; .~m2_body; .~op_body >.)
+    | false, Some m1_body, false, Some m2_body ->
+      (.< .~m2_body; .~m1_body; .~op_body >.) 
+    | _, _, false, Some m2_body -> (.< .~m2_body; .~op_body >.)
+    | false, Some m1_body, _, _ -> (.< .~m1_body; .~op_body >.)
+  in
   let init_values_body = (.<
-    let dims1 = Ndarray.dims .~n1v in
-    let dims2 = Ndarray.dims .~n2v in
-    .~n.value <- Ndarray.create dims1;
+    .~n.value <- Ndarray.create (Ndarray.dims .~n1v);
   >.) in
   (* Not required, but we preserve the order, for readability. *)
   let init_values =
@@ -81,14 +84,17 @@ let binop ~label ~name ~op_body ~grad_body m1 m2 =
   (* The code needs to be included in the reverse order to which it was computed! *)
   let grad_body = grad_body ~n1d ~n2d ~nd ~nv ~n1v ~n2v in
   let backprop_body =
-     if m1.processed && m2.processed then grad_body
-     else if m1.processed then (.< .~grad_body; .~(m2.backprop_body) >.)
-     else if m2.processed then (.< .~grad_body; .~(m1.backprop_body) >.)
-     else if l2r_comp_order then (.< .~grad_body; .~(m2.backprop_body); .~(m1.backprop_body) >.)
-     else (.< .~grad_body; .~(m1.backprop_body); .~(m2.backprop_body) >.) in
+    match m1.processed, m1.backprop_body, m2.processed, m2.backprop_body with
+    | true, _, true, _ | true, _, _, None | _, None, true, _ | _, None, _, None -> grad_body
+    | false, Some m1_body, false, Some m2_body when l2r_comp_order ->
+      (.< .~grad_body; .~m1_body; .~m2_body >.)
+    | false, Some m1_body, false, Some m2_body ->
+      (.< .~grad_body; .~m2_body; .~m1_body;  >.) 
+    | _, _, false, Some m2_body -> (.< .~grad_body; .~m2_body  >.)
+    | false, Some m1_body, _, _ -> (.< .~grad_body; .~m1_body  >.)
+    in
   let init_grads_body = (.<
-    let dims = Ndarray.dims .~nv in
-    .~n.grad <- Ndarray.create dims;
+    .~n.grad <- Ndarray.create (Ndarray.dims .~nv);
   >.) in
   (* The order is not relevant, we keep the same order as in backprop for readability. *)
   let init_grads =
@@ -106,7 +112,8 @@ let binop ~label ~name ~op_body ~grad_body m1 m2 =
       .~backprop_body
   >.) in
   m1.processed <- true; m2.processed <- true;
-  {toplevel_forward; toplevel_backprop; forward_body; backprop_body;
+  {toplevel_forward; toplevel_backprop;
+   forward_body=Some forward_body; backprop_body=Some backprop_body;
    init_values; init_grads; zero_grads;
    node_id; processed=false; debug_node}
 
@@ -120,12 +127,12 @@ let unop ~label ~name ~op_body ~grad_body m =
   let op_body = op_body ~nv ~n1v in
   (* The code needs to be included in the order it was computed! *)
   let forward_body =
-    if m.processed then op_body
-    else (.< .~(m.forward_body); .~op_body >.) in
+    match m.processed, m.forward_body with
+    | true, _ | _, None -> op_body
+    | false, Some m_body -> (.< .~m_body; .~op_body >.) in
   let init_values = (.<
     .~(m.init_values);
-    let dims = Ndarray.dims .~n1v in
-    .~n.value <- Ndarray.create dims;
+    .~n.value <- Ndarray.create (Ndarray.dims .~n1v);
   >.) in
   let toplevel_forward = (.< .~init_values; fun () -> .~forward_body >.) in
   let nd = Codelib.genlet ~name:"relud" (.< .~n.grad >.) in
@@ -139,11 +146,11 @@ let unop ~label ~name ~op_body ~grad_body m =
   let grad_body = grad_body ~n1d ~nd ~nv ~n1v in
   (* The code needs to be included in the reverse order to which it was computed! *)
   let backprop_body =
-    if m.processed then grad_body
-    else (.< .~grad_body; .~(m.backprop_body) >.) in
+    match m.processed, m.backprop_body with
+    | true, _ | _, None -> grad_body
+    | false, Some m_body -> (.< .~grad_body; .~m_body >.) in
   let init_grads_body = (.<
-    let dims = Ndarray.dims .~nv in
-    .~n.grad <- Ndarray.create dims;
+    .~n.grad <- Ndarray.create (Ndarray.dims .~nv);
   >.) in
   (* The order is not relevant, we keep the same order as in backprop for readability. *)
   let init_grads =
@@ -157,8 +164,9 @@ let unop ~label ~name ~op_body ~grad_body m =
       .~backprop_body
   >.) in
   m.processed <- true;
-  {toplevel_forward; toplevel_backprop; forward_body; backprop_body;
-  init_values; init_grads; zero_grads;
+  {toplevel_forward; toplevel_backprop;
+   forward_body=Some forward_body; backprop_body=Some backprop_body;
+   init_values; init_grads; zero_grads;
    node_id; processed=false; debug_node}
 
 (* FIXME: be careful about where n1v etc. is created vs. where it's used. *)
@@ -173,25 +181,22 @@ let param ~label ~(init_code:Ndarray.t Codelib.code) : submodel =
   let n = Codelib.genlet ~name:label (.< Node.get node_id >.) in
   let nv = Codelib.genlet ~name:(label ^ "v") (.< .~n.value >.) in
   (* Very unlikely someone will compute just the parameters. *)
-  let forward_body = (.< () >.) in
+  let forward_body = None in
   let init_values = (.<
     Hashtbl.add_exn Node.global.params ~key:label ~data:(.~n);
     .~n.value <- .~init_code;
   >.) in
-  let toplevel_forward = (.< .~init_values; fun () -> .~forward_body >.) in
-  let nd = Codelib.genlet ~name:"paramd" (.< .~n.grad >.) in
+  let toplevel_forward = (.< .~init_values; fun () -> () >.) in
+  let nd = Codelib.genlet ~name:(label^"d") (.< .~n.grad >.) in
   let zero_grads = (.< Ndarray.reset_zeros .~nd >.) in
-  let backprop_body = (.< () >.) in
+  let backprop_body = None in
   (* Very unlikely someone will want dw/dw. *)
   let init_grads = (.<
-    let dims = Ndarray.dims .~nv in
-    .~n.grad <- Ndarray.create dims;
+    .~n.grad <- Ndarray.create (Ndarray.dims .~nv);
   >.) in
   let toplevel_backprop = (.<
     .~init_grads;
-    fun () ->
-      Ndarray.reset_ones .~nd;
-      .~backprop_body
+    fun () -> Ndarray.reset_ones .~nd; ()
   >.) in
   {toplevel_forward; toplevel_backprop; forward_body; backprop_body;
     init_values; init_grads; zero_grads;
@@ -232,15 +237,46 @@ let init_uniform dims = (.< Ndarray.get_uniform ~low:(-1.0) ~high:1.0 dims >.)
 (* 
 ~/ocannl$ dune utop
 
-open Base;;
-#load "_build/default/lib/ocannl.cma";;
-open Ocannl;;
-module F = Formula;;
-let x = F.init_zeroes [|3; 3|];;
-let w = F.init_uniform [|3; 3|];;
-let nn = F.(add (mul (param ~label:"w" ~init_code:w) (param ~label:"x" ~init_code:x)) (param ~label:"b" ~init_code:b));;
-let nn_fwd = Codelib.close_code nn.toplevel_forward;;
-let nn_bwd = Codelib.close_code nn.toplevel_backprop;;
-Codelib.format_code Caml.Format.std_formatter nn_fwd;;
-Codelib.format_code Caml.Format.std_formatter nn_bwd;;
+open Base
+#load "_build/default/lib/ocannl.cma"
+open Ocannl
+module F = Formula
+let x = F.init_zeroes [|3; 3|]
+let w = F.init_uniform [|3; 3|]
+let b = F.init_uniform [|3; 3|]
+let nn = F.(add (mul (param ~label:"w" ~init_code:w) (param ~label:"x" ~init_code:x)) (param ~label:"b" ~init_code:b))
+let nn_fwd = Codelib.close_code nn.toplevel_forward
+let nn_bwd = Codelib.close_code nn.toplevel_backprop
+Codelib.format_code Caml.Format.std_formatter nn_fwd
+Codelib.format_code Caml.Format.std_formatter nn_bwd
 *)
+(* Forward:
+let addn_10 = Ocannl.Node.get 4 in
+let add1v_11 = (Ocannl.Node.get 3).Ocannl.Node.value in
+let add2v_12 = (Ocannl.Node.get 0).Ocannl.Node.value in
+let muln_5 = Ocannl.Node.get 3 in
+let mul1v_6 = (Ocannl.Node.get 2).Ocannl.Node.value in
+let mul2v_7 = (Ocannl.Node.get 1).Ocannl.Node.value in
+let w_4 = Ocannl.Node.get 2 in
+let x_3 = Ocannl.Node.get 1 in
+let b_2 = Ocannl.Node.get 0 in
+((Base.Hashtbl.add_exn Ocannl.Node.global.Ocannl.Node.params ~key:"b" ~data:b_2;
+  b_2.Ocannl.Node.value <-
+    (Ocannl.Ndarray.get_uniform ~low:(-1.0) ~high:1.0 (* CSP dims *)));
+ ((Base.Hashtbl.add_exn Ocannl.Node.global.Ocannl.Node.params ~key:"x"
+     ~data:x_3;
+   x_3.Ocannl.Node.value <-
+     ((let p_1 = Ocannl.Ndarray.create (* CSP dims *) in
+       Ocannl.Ndarray.reset_zeros p_1; p_1)));
+  (Base.Hashtbl.add_exn Ocannl.Node.global.Ocannl.Node.params ~key:"w"
+     ~data:w_4;
+   w_4.Ocannl.Node.value <-
+     (Ocannl.Ndarray.get_uniform ~low:(-1.0) ~high:1.0 (* CSP dims *)));
+  muln_5.Ocannl.Node.value <-
+    (Ocannl.Ndarray.create (Ocannl.Ndarray.dims mul1v_6)));
+ addn_10.Ocannl.Node.value <-
+   (Ocannl.Ndarray.create (Ocannl.Ndarray.dims add1v_11)));
+(fun () ->
+   Ocannl.Ndarray.assign_mul muln_5.Ocannl.Node.value mul1v_6 mul2v_7;
+   Ocannl.Ndarray.assign_add addn_10.Ocannl.Node.value add1v_11 add2v_12)
+   *)
