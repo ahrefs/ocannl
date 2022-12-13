22f7966372a6298cbc07aaf15867b8fcf86fd2c1
lukstafi <lukstafi@gmail.com>
2022-12-13 22:20:13 +0100

    The model module corresp. to "modules" in PyTorch

---
+++ lib/model.ml
@@ -0,0 +1,54 @@
+(** A model (module) is a function that takes one or more [Formula.t]s and outputs a [Formula.t],
+    while maintaining an index of trainable parameters. *)
+open Base
+
+type t = {
+  apply: Formula.t -> Formula.t;
+  params: Formula.t Sequence.t;
+}
+let linear ~w ~b =
+  let apply x = Formula.O.(w*x + b) in
+  let params = Sequence.of_list [w; b] in
+  {apply; params}
+
+let nonlinear ~w ~b =
+  let apply x = Formula.O.(!/(w*x + b)) in
+  let params = Sequence.of_list [w; b] in
+  {apply; params}
+
+let compose m1 m2 =
+  let apply x = m1.apply @@ m2.apply x in
+  let params = Sequence.merge_deduped_and_sorted m1.params m2.params
+      ~compare:(fun f1 f2 -> String.compare f1.debug_node.label f2.debug_node.label) in
+  {apply; params}
+
+let residual_compose m1 m2 =
+  let apply x = let z = m2.apply x in Formula.O.(m1.apply z + z)  in
+  let params = Sequence.merge_deduped_and_sorted m1.params m2.params
+      ~compare:(fun f1 f2 -> String.compare f1.debug_node.label f2.debug_node.label) in
+  {apply; params}
+
+module O = struct
+  include Formula.O
+  let (@@) m x = m.apply x
+  let ( % ) = compose
+  let (%+) = residual_compose
+  let ( %> ) m1 m2 = compose m2 m1
+  let (%+>) m1 m2 = residual_compose m2 m1
+end
+
+(* 
+~/ocannl$ dune utop
+
+open Base
+#load "_build/default/lib/ocannl.cma"
+open Ocannl
+module F = Formula
+let d = [|3; 3|]
+let res_mlp3 = let open Model in O.(
+    nonlinear ~w:(!~"w1" d) ~b:(!~"b1" d) %+> nonlinear ~w:(!~"w2" d) ~b:(!~"b2" d) %+>
+    linear ~w:(!~"w3" d) ~b:(!~"b3" d))
+let y = Model.O.(res_mlp3 @@ !~"x" d)
+let () = Stdio.print_endline @@ fst @@ F.sprint y.toplevel_forward
+let () = Stdio.print_endline @@ fst @@ F.sprint y.toplevel_backprop
+*)
