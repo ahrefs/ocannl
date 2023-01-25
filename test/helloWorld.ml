c584f26211b21a268d6ad1b1f54b9f2a1c79d2de
lukstafi <lukstafi@gmail.com>
2023-01-25 23:44:18 +0100

    Tutorials-like test suite `dune runtest`

---
+++ test/helloWorld.ml
@@ -0,0 +1,13 @@
+open Base
+open Ocannl
+
+let%expect_test "Hello World" =
+  Stdio.printf "Hello World!\n";
+  [%expect {| Hello World! |}]
+
+let%expect_test "Print scalar variable term" =
+  let open Operation.CLI in
+  let t = FO.(!. 7.0 *. !~ "hello_world") in
+  refresh_session ();
+  print_formula ~with_code:false ~with_grad:false `Default t;
+  [%expect {| |}]
---
+++ test/test_runtime.ml
@@ -0,0 +1,1 @@
+let () = Ocannl_runtime.Node.result__ := Some (Ocannl_runtime.Node.Obj.repr (1))
