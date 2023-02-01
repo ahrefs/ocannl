507c53e095ca6fc1ef62e615d06d0148791e6494
lukstafi <lukstafi@gmail.com>
2023-02-01 20:03:21 +0100

    Fixes #24: a small cleanup for our first test

--- test/helloWorld.ml
+++ test/helloWorld.ml
@@ -5,9 +5,14 @@ let%expect_test "Hello World" =
   Stdio.printf "Hello World!\n";
   [%expect {| Hello World! |}]
 
-let%expect_test "Print scalar variable term" =
+let%expect_test "Pointwise multiplication" =
+  Random.init 0;
   let open Operation.CLI in
-  let t = FO.(!. 7.0 *. !~ "hi") in
+  (* Hey is inferred to be a scalar. *)
+  let t = FO.(!. 2.0  *. !~ "hey") in
   refresh_session ();
   print_formula ~with_code:false ~with_grad:false `Default t;
-  [%expect {| |}]
+  [%expect {|
+    [4] heyv2p: 1
+    dims: (1)
+    0=0   -1.334558 |}]
