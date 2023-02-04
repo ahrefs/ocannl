48e44a104df25f10ced6da3b42f0ca856d4834fe
lukstafi <lukstafi@gmail.com>
2023-02-04 19:32:37 +0100

    Rename `apply` field/type to `comp`, cleanups, new `apply_two`, better `return_term`.

--- lib/network.ml
+++ lib/network.ml
@@ -1,78 +1,92 @@
-(** A network (module) is a [Formula.t], or a function that takes [Formula.t]s and outputs a [Formula.t],
+(** A network (module) is a [F.t], or a function that takes [F.t]s and outputs a [F.t],
     while maintaining an index of trainable parameters. *)
 open Base
+module F = Formula
 
 (* FIXME(28): implement [promote_precision] effects. *)
 
 (** Composable network components, with parameter tracking. *)
 type 'a t = {
-  apply: 'a apply;
-  params: (Formula.t, Formula.comparator_witness) Set.t;
-  promote_precision: Ndcode.precision option;
+  comp: 'a comp;
+  (** The parametric computation. *)
+  params: (F.t, F.comparator_witness) Set.t;
+  mutable promote_precision: Ndcode.precision option;
   (** The precision at which the network's computation should happen, regardless of the precisions
       of the inputs and results, unless otherwise specified in subnetworks. *)
 }
-and _ apply =
-| Nullary: Formula.t -> Formula.t apply
-| Unary: (Formula.t -> Formula.t) -> (Formula.t -> Formula.t) apply
-| Binary: (Formula.t -> Formula.t -> Formula.t) -> (Formula.t -> Formula.t -> Formula.t) apply
+and _ comp =
+| Nullary: F.t -> F.t comp
+| Unary: (F.t -> F.t) -> (F.t -> F.t) comp
+| Binary: (F.t -> F.t -> F.t) -> (F.t -> F.t -> F.t) comp
 
-let apply_val (type a) (n : a t): a =
-  match n.apply with
+let unpack (type a) (n : a t): a =
+  match n.comp with
   | Nullary f -> f
   | Unary f -> f
   | Binary f -> f
 
-let partial (type a) (n : (Formula.t -> a) t) x: a t =
-  match n.apply with
-  | Unary f -> {n with apply=Nullary (f x)}
-  | Binary f -> {n with apply=Unary (f x)}
-
-let apply_nullary ?promote_precision (type a) (f: (Formula.t -> a) t) (x: Formula.t t): a t =
-  let apply = 
-    match f.apply, x.apply with
-    | Unary f, Nullary x -> (Nullary (f x): a apply)
+let apply (type a) (f: (F.t -> a) t) (x: F.t t): a t =
+  let comp = 
+    match f.comp, x.comp with
+    | Unary f, Nullary x -> (Nullary (f x): a comp)
     | Binary f, Nullary x -> Unary (f x) in
   let params = Set.union f.params x.params in
-  {apply; params; promote_precision}
+  {comp; params; promote_precision=None}
 
-let compose ?promote_precision (type a) (f: (Formula.t -> a) t) (g: (Formula.t -> a) t): (Formula.t -> a) t =
-  let apply = 
-    match f.apply, g.apply with
-    | Unary f, Unary g -> (Unary (fun x -> f (g x)): (Formula.t -> a) apply)
-    | Binary f, Binary g -> (Binary (fun x y -> f (g x y) y): (Formula.t -> a) apply) in
+(** Note that [apply_two f x1 x2 = apply (apply f x1) x2]. *)
+let apply_two (type a) (f: (F.t -> F.t -> a) t) (x1: F.t t) (x2: F.t t): a t =
+  let comp = 
+    match f.comp, x1.comp, x2.comp with
+    | Binary f, Nullary x1, Nullary x2 -> (Nullary (f x1 x2): a comp) in
+  let params = Set.union_list (module F) [f.params; x1.params; x2.params] in
+  {comp; params; promote_precision=None}
+
+let compose (type a) (f: (F.t -> a) t) (g: (F.t -> a) t): (F.t -> a) t =
+  let comp = 
+    match f.comp, g.comp with
+    | Unary f, Unary g -> (Unary (fun x -> f (g x)): (F.t -> a) comp)
+    | Binary f, Binary g -> (Binary (fun x y -> f (g x y) y): (F.t -> a) comp) in
   let params = Set.union f.params g.params in
-  {apply; params; promote_precision}
+  {comp; params; promote_precision=None}
 
-let swap (type a) (f: (Formula.t -> Formula.t -> a) t) =
-  let apply = 
-    match f.apply with
-    | Binary f -> (Binary (fun x y -> f y x): (Formula.t -> Formula.t -> a) apply) in
-  {f with apply}
+let swap (type a) (f: (F.t -> F.t -> a) t) =
+  let comp = 
+    match f.comp with
+    | Binary f -> (Binary (fun x y -> f y x): (F.t -> F.t -> a) comp) in
+  {f with comp}
 
-let bind_ret (type a) (m: a t) (f: Formula.t -> a) =
-  let apply = 
-    match m.apply with
-    | Nullary g -> (Nullary (f g): a apply)
+let bind_ret (type a) (m: a t) (f: F.t -> a) =
+  let comp = 
+    match m.comp with
+    | Nullary g -> (Nullary (f g): a comp)
     | Unary g -> Unary (fun x -> f (g x) x)
     | Binary g -> Binary (fun x y -> f (g x y) x y) in
-  {m with apply}
+  {m with comp}
 
+(** Caution! [return_term] should only be used with terminals. *)
+let return_term x =
+  let params =
+    if x.F.needs_gradient then Set.singleton (module F) x else Set.empty (module F) in
+  {comp=Nullary x; params; promote_precision=None}
+
+let return c =
+  {comp=c; params=Set.empty (module F); promote_precision=None}
+  
 module FO = Operation.O
 
-let residual_compose ?promote_precision (type a) (f: (Formula.t -> a) t) (g: (Formula.t -> a) t): (Formula.t -> a) t =
-  let apply = 
-    match f.apply, g.apply with
-    | Unary f, Unary g -> (Unary FO.(fun x -> let y = g x in f y + y): (Formula.t -> a) apply)
-    | Binary f, Binary g -> (Binary FO.(fun x y -> let z = g x y in f z y + z): (Formula.t -> a) apply) in
+let residual_compose (type a) (f: (F.t -> a) t) (g: (F.t -> a) t): (F.t -> a) t =
+  let comp = 
+    match f.comp, g.comp with
+    | Unary f, Unary g -> (Unary FO.(fun x -> let y = g x in f y + y): (F.t -> a) comp)
+    | Binary f, Binary g -> (Binary FO.(fun x y -> let z = g x y in f z y + z): (F.t -> a) comp) in
   let params = Set.union f.params g.params in
-  {apply; params; promote_precision}
+  {comp; params; promote_precision=None}
 
 let sum_over_params n ~f = Set.sum (module Operation.Summable) n.params ~f
 
 module O = struct
   include Operation.O
-  let (@@) m x = apply_nullary m x
+  let (@@) m x = apply m x
   let ( % ) = compose
   let (%+) = residual_compose
   (* This is like [(>>>)] from arrows, but [(%>)] is neater. *)
