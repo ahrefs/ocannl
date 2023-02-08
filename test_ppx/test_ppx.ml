aa452c22631be8867909833b8dd0df0cefb74cd8
lukstafi <lukstafi@gmail.com>
2023-02-08 12:41:01 +0100

    Fix: Module as standalone test

--- test_ppx/test_ppx.ml
+++ test_ppx/test_ppx.ml
@@ -1,5 +1,9 @@
-let%ocannl y0 = 2 * "hey" + 3
-(*  *)
-let%ocannl y x = 2 * "hey" + x
+open Ocannl
 
-let%ocannl y x1 x2 = x1 * "hey" + x2
+let%ocannl y0 = 2 * "hey" + 3
+
+let%ocannl y1 x = 2 * "hey" + x
+
+let%ocannl y2 x1 x2 = x1 * "hey" + x2
+
+let () = ignore (y0, y1, y2)
