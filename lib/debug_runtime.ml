aaafa8dc679ef08408296d06272c93cc146e4eb1
lukstafi <lukstafi@gmail.com>
2023-02-23 13:04:23 +0100

    `ppx_minidebug`: ack `Ptyp_poly` and `Ppat_constraint` Weren't expecting them in some places: pass through. Also, generalize by allowing debugged binding LHS to be alias patterns, not just identifiers.

--- lib/debug_runtime.ml
+++ lib/debug_runtime.ml
@@ -2,10 +2,13 @@ open Base
 
 (* FIXME: un-hardcode the path *)
 let debug_ch =
-  let result =
+  let debug_ch =
     Stdio.Out_channel.create ~binary:false ~append:true "/home/lukstafi/ocannl/debugger.log" in
-  Stdio.Out_channel.fprintf result "\nBEGIN DEBUG SESSION at time UTC %s\n%!"
+  Stdio.Out_channel.fprintf debug_ch "\nBEGIN DEBUG SESSION at time UTC %s\n%!"
     (Core.Time_ns.to_string_utc @@ Core.Time_ns.now());
-  result
+  debug_ch
 
-let ppf = Caml.Format.formatter_of_out_channel debug_ch
+let ppf =
+  let ppf = Caml.Format.formatter_of_out_channel debug_ch in
+  Caml.Format.pp_set_geometry ppf ~max_indent:50 ~margin:100;
+  ppf
--- lib/ppx_minidebug.ml
+++ lib/ppx_minidebug.ml
@@ -4,21 +4,27 @@ open Ppxlib
 
 module A = Ast_builder.Default
 
-let pat2expr pat =
+let rec pat2expr pat =
   let loc = pat.ppat_loc in
   match pat.ppat_desc with
-  | Ppat_var ident -> Ast_builder.Default.pexp_ident ~loc {ident with txt = Lident ident.txt}
+  | Ppat_constraint (pat', typ) ->
+    Ast_builder.Default.pexp_constraint ~loc (pat2expr pat') typ
+  | Ppat_alias (_, ident)
+  | Ppat_var ident ->
+    Ast_builder.Default.pexp_ident ~loc {ident with txt = Lident ident.txt}
   | _ ->
      Ast_builder.Default.pexp_extension ~loc @@ Location.error_extensionf ~loc
-       "OCaNNL currently only supports single identifiers as argument patterns."
+       "ppx_minidebug requires a pattern identifier here: try using an `as` alias."
 
-let pat2pat_res pat =
+let rec pat2pat_res pat =
   let loc = pat.ppat_loc in
   match pat.ppat_desc with
+  | Ppat_constraint (pat', _) -> pat2pat_res pat'
+  | Ppat_alias (_, ident)
   | Ppat_var ident -> Ast_builder.Default.ppat_var ~loc {ident with txt = ident.txt ^ "__res"}
   | _ ->
-     Ast_builder.Default.ppat_extension ~loc @@ Location.error_extensionf ~loc
-       "OCaNNL currently only supports single identifiers as argument patterns."
+    Ast_builder.Default.ppat_extension ~loc @@ Location.error_extensionf ~loc
+      "ppx_minidebug requires a pattern identifier here: try using an `as` alias."
 
 let rec splice_lident ~id_prefix = function
   | Lident id -> Lident (id_prefix ^ id)
@@ -30,7 +36,7 @@ let log_preamble ?(message="") ~loc () =
   [%expr
     let fname = [%e A.estring ~loc loc.loc_start.pos_fname] in
     Caml.Format.fprintf Debug_runtime.ppf
-      "\"%s\":%d:%d-%d:%d@ at time UTC@ %s: %s@ " fname
+      "@[\"%s\":%d:%d-%d:%d@ at time UTC@ %s: %s@]" fname
       [%e A.eint ~loc loc.loc_start.pos_lnum]
       [%e A.eint ~loc (loc.loc_start.pos_cnum - loc.loc_start.pos_bol)]
       [%e A.eint ~loc loc.loc_end.pos_lnum]
@@ -42,15 +48,18 @@ let log_value ~loc ~t_lident_loc ~descr_loc exp =
   let converter = A.pexp_ident ~loc 
       {t_lident_loc with txt = splice_lident ~id_prefix:"sexp_of_" t_lident_loc.txt} in
   [%expr
-     [%e log_preamble ~loc ()];
-     Caml.Format.fprintf Debug_runtime.ppf "%s =@ %a"
+     [%e log_preamble ~loc:t_lident_loc.loc ()];
+     Caml.Format.fprintf Debug_runtime.ppf "%s =@ %a@ "
        [%e A.estring ~loc:descr_loc.loc descr_loc.txt] Sexp.pp_hum ([%e converter] [%e exp])]
 
 exception Not_transforming
 
 let rec collect_fun accu = function
   | [%expr fun [%p? arg] -> [%e? body]] as exp -> collect_fun ((arg, exp.pexp_loc)::accu) body
-  | [%expr ([%e? body] : [%t? {ptyp_desc=Ptyp_constr (t_lident_loc, []); _} as typ ] ) ] ->
+  | [%expr ([%e? body] :
+              [%t? {ptyp_desc=(
+                  Ptyp_constr (t_lident_loc, []) | Ptyp_poly (_, {ptyp_desc=Ptyp_constr (t_lident_loc, []); _})
+                ); _} as typ ] ) ] ->
     List.rev accu, body, Some t_lident_loc, Some typ
   | body -> List.rev accu, body, None, None
 
@@ -60,27 +69,30 @@ let rec expand_fun body = function
 
 let debug_fun callback bind descr_loc t_lident_loc_opt1 exp =
   let args, body, t_lident_loc_opt2, typ_opt = collect_fun [] exp in
+  let loc = exp.pexp_loc in
   let t_lident_loc =
     match t_lident_loc_opt1, t_lident_loc_opt2 with
     | Some t_lident_loc, _ | None, Some t_lident_loc -> t_lident_loc
     | None, None -> raise Not_transforming in
   let arg_logs = List.filter_map args ~f:(function
       | [%pat? ([%p? {ppat_desc=Ppat_var descr_loc; _} as pat ] :
-                  [%t? {ptyp_desc=Ptyp_constr (t_lident_loc, []); _} ] ) ], loc ->
+                  [%t? {ptyp_desc=(
+                      Ptyp_constr (t_lident_loc, [])
+                    | Ptyp_poly (_, {ptyp_desc=Ptyp_constr (t_lident_loc, []); _})
+                    ); _} ] ) ], loc ->
         Some (log_value ~loc ~t_lident_loc ~descr_loc (pat2expr pat))
       | _ -> None
   ) in
-  let loc = exp.pexp_loc in
   let init = log_preamble ~message:descr_loc.txt ~loc () in
   let arg_logs = List.fold arg_logs ~init ~f:(fun e1 e2 -> [%expr [%e e1]; [%e e2]]) in
   let result = pat2pat_res bind in
   let body =
     [%expr
-      Caml.Format.pp_open_box Debug_runtime.ppf 2;
+      Caml.Format.pp_open_hvbox Debug_runtime.ppf 2;
       [%e arg_logs];
       let [%p result] = [%e callback body] in
       [%e log_value ~loc ~t_lident_loc ~descr_loc (pat2expr result)];
-      Caml.Format.pp_close_box Debug_runtime.ppf;
+      Caml.Format.pp_close_box Debug_runtime.ppf ();
       [%e pat2expr result]] in
   let body =
     match typ_opt with None -> body | Some typ -> [%expr ([%e body] : [%t typ])] in
@@ -88,15 +100,22 @@ let debug_fun callback bind descr_loc t_lident_loc_opt1 exp =
 
 let debug_binding callback vb =
   let pat = vb.pvb_pat in
-  let descr_loc, t_lident_loc_opt =
-    match vb.pvb_pat with
-    | [%pat? ([%p? {ppat_desc=Ppat_var descr_loc; _} ] :
-                [%t? {ptyp_desc=Ptyp_constr (t_lident_loc, []); _} ] ) ] ->
-      descr_loc, Some t_lident_loc
-    | {ppat_desc=Ppat_var descr_loc; _} ->
-      descr_loc, None
-    | _ -> raise Not_transforming in
   let loc = vb.pvb_loc in
+  let descr_loc, t_lident_loc_opt =
+    match vb.pvb_pat, vb.pvb_expr with
+    | [%pat? ([%p? {ppat_desc=Ppat_var descr_loc; _} ] :
+                [%t? {ptyp_desc=(
+                    Ptyp_constr (t_lident_loc, []) | Ptyp_poly (_, {ptyp_desc=Ptyp_constr (t_lident_loc, []); _})
+                ); _} ] ) ], _ ->
+      descr_loc, Some t_lident_loc
+    | {ppat_desc=Ppat_var descr_loc; _}, 
+      [%expr ([%e? _exp] : [%t? {ptyp_desc=(
+        Ptyp_constr (t_lident_loc, []) | Ptyp_poly (_, {ptyp_desc=Ptyp_constr (t_lident_loc, []); _})
+      ); _} ])] ->
+        descr_loc, Some t_lident_loc
+    | {ppat_desc=Ppat_var descr_loc; _}, _ ->
+        descr_loc, None
+    | _ -> (* raise Not_transforming *)  {txt="made_up_d_2"; loc}, None in
   match vb.pvb_expr.pexp_desc, t_lident_loc_opt with
   | Pexp_fun _, _ -> 
     {vb with pvb_expr = debug_fun callback vb.pvb_pat descr_loc t_lident_loc_opt vb.pvb_expr}
@@ -104,11 +123,11 @@ let debug_binding callback vb =
     let result = pat2pat_res pat in
     let exp =
       [%expr
-        Caml.Format.pp_open_box Debug_runtime.ppf 2;
-        [%e log_preamble ~message:descr_loc.txt ~loc ()];
+        Caml.Format.pp_open_hvbox Debug_runtime.ppf 2;
+        [%e log_preamble ~message:descr_loc.txt ~loc:descr_loc.loc ()];
         let [%p result] = [%e callback vb.pvb_expr] in
-        [%e log_value ~loc ~t_lident_loc ~descr_loc (pat2expr pat)];
-        Caml.Format.pp_close_box Debug_runtime.ppf;
+        [%e log_value ~loc ~t_lident_loc ~descr_loc (pat2expr result)];
+        Caml.Format.pp_close_box Debug_runtime.ppf ();
         [%e pat2expr result]] in
     {vb with pvb_expr = exp}
   | _ -> raise Not_transforming
@@ -139,22 +158,16 @@ let traverse =
       | _ -> super#structure_item si
   end
 
-let debug_expander ~ctxt:_ payload =
+let debug_this_expander ~ctxt:_ payload =
+  let callback e = traverse#expression e in
   match payload with
   | { pexp_desc = Pexp_let (recflag, bindings, body); _ } ->
-    (* This is the [let%debug ... in] use-case: do not debug the whole body. *)
-     let bindings = List.map bindings ~f:(debug_binding (fun e -> traverse#expression e)) in
+    (* This is the [let%debug_this ... in] use-case: do not debug the whole body. *)
+     let bindings = List.map bindings ~f:(debug_binding callback) in
      {payload with pexp_desc=Pexp_let (recflag, bindings, body)}
   | expr -> expr
 
-(*let debug_with_trace_expander ~ctxt payload =
-  [%expr
-    let backtrace = Caml.Printexc.get_callstack 5 in
-    Caml.Printexc.print_raw_backtrace Stdio.stdout backtrace;
-    [%e debug_expander ~ctxt payload]
-  ]*)
-
-let debug_this_expander ~ctxt:_ payload = traverse#expression payload
+let debug_expander ~ctxt:_ payload = traverse#expression payload
 
 let str_expander ~loc ~path:_ payload =
   match List.map payload ~f:(fun si -> traverse#structure_item si) with
