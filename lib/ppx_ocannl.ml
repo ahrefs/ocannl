7627585d5f58179490ffc5d82cdf5c4614cf1dcd
lukstafi <lukstafi@gmail.com>
2023-03-09 23:13:37 +0100

    Fixes #45: param punning i.e. generate bindings Strings are converted to let-bindings in an encompassing scope instead of to parameter formulas directly.

--- lib/ppx_ocannl.ml
+++ lib/ppx_ocannl.ml
@@ -125,46 +125,61 @@ let ndarray_constant ?axis_labels ?label expr =
       ([%e op] ~batch_dims:[%e edims batch_dims] ~input_dims:[%e edims input_dims]
          ~output_dims:[%e edims output_dims] [%e values])]
 
+let let_opt ~loc vbs expr =
+  if List.is_empty vbs then expr
+  else Ast_helper.Exp.let_ ~loc Nonrecursive vbs expr
+         
 let rec translate expr =
   let loc = expr.pexp_loc in
   match expr with
   | { pexp_desc = Pexp_constant (Pconst_float _); _ } ->
-    [%expr Network.return_term (Operation.number [%e expr])]
+    [], [%expr Network.return_term (Operation.number [%e expr])]
 
   | { pexp_desc = Pexp_constant (Pconst_integer _); _ } ->
-    [%expr Network.return_term (Operation.number (Float.of_int [%e expr]))]
+    [], [%expr Network.return_term (Operation.number (Float.of_int [%e expr]))]
 
   | [%expr [%e? { pexp_desc = Pexp_constant (Pconst_string _); _ } as s]
       [%e? { pexp_desc = Pexp_constant (Pconst_float _); _ } as f]] ->
-    [%expr Network.return_term (Operation.number ~axis_label:[%e s] [%e f])]
+    [], [%expr Network.return_term (Operation.number ~axis_label:[%e s] [%e f])]
 
   | [%expr [%e? { pexp_desc = Pexp_constant (Pconst_string _); _ } as s]
       [%e? { pexp_desc = Pexp_constant (Pconst_integer _); _ } as i]] ->
-    [%expr Network.return_term (Operation.number ~axis_label:[%e s] (Float.of_int [%e i]))]
+    [], [%expr Network.return_term (Operation.number ~axis_label:[%e s] (Float.of_int [%e i]))]
 
-  | { pexp_desc = Pexp_constant (Pconst_string _); _ } ->
-    [%expr Network.return_term Operation.O.(!~ [%e expr])]
+  | { pexp_desc = Pexp_constant (Pconst_string (ident, str_loc, _)); _ } ->
+    let pat = Ast_helper.Pat.var ~loc {loc=str_loc; txt=ident} in
+    let v = [%expr Network.return_term Operation.O.(!~ [%e expr])] in
+    let vb = Ast_helper.Vb.mk ~loc pat v in
+    [vb], pat2expr pat
 
   | { pexp_desc = Pexp_tuple _; _ } | { pexp_desc = Pexp_array _; _ } 
-  | { pexp_desc = Pexp_construct ({txt=Lident "::"; _}, _); _ } -> ndarray_constant expr
+  | { pexp_desc = Pexp_construct ({txt=Lident "::"; _}, _); _ } ->
+    [], ndarray_constant expr
     
   | [%expr [%e? expr1] [%e? expr2] [%e? expr3] ] ->
-    [%expr Network.apply (Network.apply [%e translate expr1] [%e translate expr2]) [%e translate expr3]]
+    let vbs1, e1 = translate expr1 in
+    let vbs2, e2 = translate expr2 in
+    let vbs3, e3 = translate expr3 in
+    vbs1 @ vbs2 @ vbs3, [%expr Network.apply (Network.apply [%e e1] [%e e2]) [%e e3]]
 
   | [%expr [%e? expr1] [%e? expr2] ] ->
-    [%expr Network.apply [%e translate expr1] [%e translate expr2]]
+    let vbs1, e1 = translate expr1 in
+    let vbs2, e2 = translate expr2 in
+    vbs1 @ vbs2, [%expr Network.apply [%e e1] [%e e2]]
 
   | [%expr fun ~config [%p? pat1] [%p? pat2] -> [%e? body] ] ->
     (* TODO(#38): generalize config to any number of labeled arguments with any labels. *)
     let pat1_ref = pat2pat_ref pat1 in
     let pat2_ref = pat2pat_ref pat2 in
-    [%expr
+    let vbs, body = translate body in
+    let body = let_opt ~loc vbs body in
+    [], [%expr
       fun ~config ->
         let [%p pat1_ref] = ref [] in
         let [%p pat1] = Network.return (Network.Placeholder [%e pat2expr @@ pat1_ref]) in
         let [%p pat2_ref] = ref [] in
         let [%p pat2] = Network.return (Network.Placeholder [%e pat2expr @@ pat2_ref]) in
-        let body = [%e translate body] in
+        let body = [%e body] in
         fun [%p pat1] [%p pat2] ->
           [%e pat2expr pat1_ref] := [%e pat2expr pat1] :: ![%e pat2expr pat1_ref];
           [%e pat2expr pat2_ref] := [%e pat2expr pat2] :: ![%e pat2expr pat2_ref];
@@ -177,11 +192,13 @@ let rec translate expr =
   | [%expr fun ~config [%p? pat] -> [%e? body] ] ->
     (* TODO(#38): generalize config to any number of labeled arguments with any labels. *)
     let pat_ref = pat2pat_ref pat in
-    [%expr
+    let vbs, body = translate body in
+    let body = let_opt ~loc vbs body in
+    [], [%expr
       fun ~config ->
         let [%p pat_ref] = ref [] in
         let [%p pat] = Network.return (Network.Placeholder [%e pat2expr @@ pat_ref]) in
-        let body = [%e translate body] in
+        let body = [%e body] in
         fun [%p pat] ->
           [%e pat2expr pat_ref] := [%e pat2expr pat] :: ![%e pat2expr pat_ref];
           let result__ = Network.unpack body in
@@ -192,12 +209,13 @@ let rec translate expr =
   | [%expr fun [%p? pat1] [%p? pat2] -> [%e? body] ] ->
     let pat1_ref = pat2pat_ref pat1 in
     let pat2_ref = pat2pat_ref pat2 in
-    [%expr
+    let vbs, body = translate body in
+    vbs, [%expr
       let [%p pat1_ref] = ref [] in
       let [%p pat1] = Network.return (Network.Placeholder [%e pat2expr @@ pat1_ref]) in
       let [%p pat2_ref] = ref [] in
       let [%p pat2] = Network.return (Network.Placeholder [%e pat2expr @@ pat2_ref]) in
-      let body = [%e translate body] in
+      let body = [%e body] in
       fun [%p pat1] [%p pat2] ->
         [%e pat2expr pat1_ref] := [%e pat2expr pat1] :: ![%e pat2expr pat1_ref];
         [%e pat2expr pat2_ref] := [%e pat2expr pat2] :: ![%e pat2expr pat2_ref];
@@ -209,10 +227,11 @@ let rec translate expr =
 
   | [%expr fun [%p? pat] -> [%e? body] ] ->
     let pat_ref = pat2pat_ref pat in
-    [%expr
+    let vbs, body = translate body in
+    vbs, [%expr
       let [%p pat_ref] = ref [] in
       let [%p pat] = Network.return (Network.Placeholder [%e pat2expr @@ pat_ref]) in
-      let body = [%e translate body] in
+      let body = [%e body] in
       fun [%p pat] ->
         [%e pat2expr pat_ref] := [%e pat2expr pat] :: ![%e pat2expr pat_ref];
         let result__ = Network.unpack body in
@@ -221,53 +240,72 @@ let rec translate expr =
   ]
 
   | [%expr while [%e? test_expr] do [%e? body_expr] done ] ->
-    [%expr while [%e test_expr] do [%e translate body_expr] done ]
+    let vbs, body = translate body_expr in
+    vbs, [%expr while [%e test_expr] do [%e body] done ]
 
-  | [%expr for [%p? pat] = [%e? init] to [%e? final] do [%e? body] done ] ->
-    [%expr for [%p pat] = [%e init] to [%e final] do [%e translate body] done ]
+  | [%expr for [%p? pat] = [%e? init] to [%e? final] do [%e? body_expr] done ] ->
+    let vbs, body = translate body_expr in
+    vbs, [%expr for [%p pat] = [%e init] to [%e final] do [%e body] done ]
 
-  | [%expr for [%p? pat] = [%e? init] downto [%e? final] do [%e? body] done ] ->
-    [%expr for [%p pat] = [%e init] downto [%e final] do [%e translate body] done ]
+  | [%expr for [%p? pat] = [%e? init] downto [%e? final] do [%e? body_expr] done ] ->
+    let vbs, body = translate body_expr in
+    vbs, [%expr for [%p pat] = [%e init] downto [%e final] do [%e body] done ]
 
   | [%expr [%e? expr1] ; [%e? expr2] ] ->
-    [%expr [%e translate expr1] ; [%e translate expr2]]
+    let vbs1, e1 = translate expr1 in
+    let vbs2, e2 = translate expr2 in
+    vbs1 @ vbs2, [%expr [%e e1] ; [%e e2]]
 
   | [%expr if [%e? expr1] then [%e? expr2] else [%e? expr3]] ->
-    [%expr if [%e expr1] then [%e translate expr2] else [%e translate expr3]]
+    let vbs2, e2 = translate expr2 in
+    let vbs3, e3 = translate expr3 in
+    vbs2 @ vbs3, [%expr if [%e expr1] then [%e e2] else [%e e3]]
 
   | [%expr if [%e? expr1] then [%e? expr2]] ->
-    [%expr if [%e expr1] then [%e translate expr2]]
+    let vbs2, e2 = translate expr2 in
+    vbs2, [%expr if [%e expr1] then [%e e2]]
 
   | { pexp_desc = Pexp_match (expr1, cases); _ } ->
-    let cases =
-       List.map cases
+    let vbss, cases =
+       List.unzip @@ List.map cases
          ~f:(fun ({pc_rhs; _} as c) ->
-            {c with pc_rhs=translate pc_rhs}) in
-     { expr with pexp_desc = Pexp_match (expr1, cases) }
+            let vbs, pc_rhs = translate pc_rhs in
+            vbs, {c with pc_rhs}) in
+     List.concat vbss, { expr with pexp_desc = Pexp_match (expr1, cases) }
 
   | { pexp_desc = Pexp_let (recflag, bindings, body); _ } ->
-     let body = translate body in
-     let bindings = List.map bindings ~f:(fun binding ->
-         {binding with pvb_expr = translate binding.pvb_expr}) in
-     {expr with pexp_desc=Pexp_let (recflag, bindings, body)}
+     let vbss1, bindings = List.unzip @@ List.map bindings
+         ~f:(fun binding ->
+          let vbs, pvb_expr = translate binding.pvb_expr in
+          vbs, {binding with pvb_expr}) in
+     let vbs2, body = translate body in
+     let all_bindings = List.concat (vbss1 @ [bindings; vbs2]) in
+     [], {expr with pexp_desc=Pexp_let (recflag, all_bindings, body)}
 
   | { pexp_desc = Pexp_open (decl, body); _ } ->
-     {expr with pexp_desc=Pexp_open (decl, translate body)}
+    let vbs, body = translate body in
+    vbs, {expr with pexp_desc=Pexp_open (decl, body)}
 
   | { pexp_desc = Pexp_letmodule (name, module_expr, body); _ } ->
-     {expr with pexp_desc=Pexp_letmodule (name, module_expr, translate body)}
+    let vbs, body = translate body in
+     vbs, {expr with pexp_desc=Pexp_letmodule (name, module_expr, body)}
 
   | expr ->
-    expr
+    [], expr
 
 let expr_expander ~loc ~path:_ payload =
   match payload with
   | { pexp_desc = Pexp_let (recflag, bindings, body); _ } ->
     (* We are at the %ocannl annotation level: do not tranlsate the body. *)
-     let bindings = List.map bindings ~f:(fun vb ->
-         {vb with pvb_expr=[%expr let open! Network.O in [%e translate vb.pvb_expr]]}) in
-     {payload with pexp_desc=Pexp_let (recflag, bindings, body)}
-  | expr -> translate expr
+     let vbss, bindings = List.unzip @@ List.map bindings
+      ~f:(fun vb ->
+        let vbs, v = translate vb.pvb_expr in
+        vbs, {vb with pvb_expr=[%expr let open! Network.O in [%e v]]}) in
+     let expr = {payload with pexp_desc=Pexp_let (recflag, bindings, body)} in
+     let_opt ~loc (List.concat vbss) expr
+  | expr ->
+    let vbs, expr = translate expr in
+    let_opt ~loc vbs expr
 
 let flatten_str ~loc ~path:_ items =
   match items with
@@ -278,14 +316,19 @@ let flatten_str ~loc ~path:_ items =
      ; pincl_loc = loc
      ; pincl_attributes = [] }
 
-let translate_str ({pstr_desc; _} as str) =
+let translate_str ({pstr_desc; pstr_loc=loc; _} as str) =
   match pstr_desc with
-  | Pstr_eval (expr, attrs) -> {str with pstr_desc=Pstr_eval (translate expr, attrs)}
-  | Pstr_value (recf, vbl) ->
+  | Pstr_eval (expr, attrs) ->
+    let vbs, expr = translate expr in
+    let expr = let_opt ~loc vbs expr in
+    {str with pstr_desc=Pstr_eval (expr, attrs)}
+  | Pstr_value (recf, bindings) ->
     let f vb =
       let loc = vb.pvb_loc in
-      {vb with pvb_expr=[%expr let open! Network.O in [%e translate vb.pvb_expr]]} in
-    {str with pstr_desc=Pstr_value (recf, List.map vbl ~f)}
+      let vbs, v = translate vb.pvb_expr in
+      let v = let_opt ~loc vbs v in
+      {vb with pvb_expr=[%expr let open! Network.O in [%e v]]} in
+    {str with pstr_desc=Pstr_value (recf, List.map bindings ~f)}
   | _ -> str
      
 let str_expander ~loc ~path (payload: structure_item list) =
--- test_ppx/test_ppx_expected.ml
+++ test_ppx/test_ppx_expected.ml
@@ -2,15 +2,16 @@ open Base
 open Ocannl
 let y0 =
   let open! Network.O in
+    let hey = Network.return_term (let open Operation.O in !~ "hey") in
     Network.apply
       (Network.apply (+)
          (Network.apply
             (Network.apply ( * )
-               (Network.return_term (Operation.number (Float.of_int 2))))
-            (Network.return_term (let open Operation.O in !~ "hey"))))
+               (Network.return_term (Operation.number (Float.of_int 2)))) hey))
       (Network.return_term (Operation.number (Float.of_int 3)))
 let y1 =
   let open! Network.O in
+    let hey = Network.return_term (let open Operation.O in !~ "hey") in
     let x__ref = ref [] in
     let x = Network.return (Network.Placeholder x__ref) in
     let body =
@@ -19,22 +20,21 @@ let y1 =
            (Network.apply
               (Network.apply ( * )
                  (Network.return_term (Operation.number (Float.of_int 2))))
-              (Network.return_term (let open Operation.O in !~ "hey")))) x in
+              hey)) x in
     fun x ->
       x__ref := (x :: (!x__ref));
       (let result__ = Network.unpack body in
        x__ref := (List.tl_exn (!x__ref)); result__)
 let y2 =
   let open! Network.O in
+    let hey = Network.return_term (let open Operation.O in !~ "hey") in
     let x1__ref = ref [] in
     let x1 = Network.return (Network.Placeholder x1__ref) in
     let x2__ref = ref [] in
     let x2 = Network.return (Network.Placeholder x2__ref) in
     let body =
       Network.apply
-        (Network.apply (+)
-           (Network.apply (Network.apply ( * ) x1)
-              (Network.return_term (let open Operation.O in !~ "hey")))) x2 in
+        (Network.apply (+) (Network.apply (Network.apply ( * ) x1) hey)) x2 in
     fun x1 ->
       fun x2 ->
         x1__ref := (x1 :: (!x1__ref));
@@ -59,11 +59,12 @@ let b =
                                                                  10)|])
 let y =
   let open! Network.O in
+    let hey = Network.return_term (let open Operation.O in !~ "hey") in
     Network.apply
       (Network.apply (+)
          (Network.apply
             (Network.apply ( * )
                (Network.return_term (Operation.number ~axis_label:"q" 2.0)))
-            (Network.return_term (let open Operation.O in !~ "hey"))))
+            hey))
       (Network.return_term (Operation.number ~axis_label:"p" 1.0))
 let () = ignore (y0, y1, y2, a, b, y)
