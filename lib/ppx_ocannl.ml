9528f813f16cec35f9ea1f93430c6a563ea7d280
lukstafi <lukstafi@gmail.com>
2023-03-10 11:54:28 +0100

    Fix param punning: collapse repeated introductions

--- lib/ppx_ocannl.ml
+++ lib/ppx_ocannl.ml
@@ -126,46 +126,51 @@ let ndarray_constant ?axis_labels ?label expr =
          ~output_dims:[%e edims output_dims] [%e values])]
 
 let let_opt ~loc vbs expr =
-  if List.is_empty vbs then expr
-  else Ast_helper.Exp.let_ ~loc Nonrecursive vbs expr
-         
+  if Map.is_empty vbs then expr
+  else Ast_helper.Exp.let_ ~loc Nonrecursive (Map.data vbs) expr
+
+let no_vbs = Map.empty (module String)
+let reduce_vbss = List.reduce_exn ~f:(Map.merge_skewed ~combine:(fun ~key:_ _v1 v2 -> v2))
+
 let rec translate expr =
   let loc = expr.pexp_loc in
   match expr with
   | { pexp_desc = Pexp_constant (Pconst_float _); _ } ->
-    [], [%expr Network.return_term (Operation.number [%e expr])]
+    no_vbs, [%expr Network.return_term (Operation.number [%e expr])]
 
   | { pexp_desc = Pexp_constant (Pconst_integer _); _ } ->
-    [], [%expr Network.return_term (Operation.number (Float.of_int [%e expr]))]
+    no_vbs, [%expr Network.return_term (Operation.number (Float.of_int [%e expr]))]
 
   | [%expr [%e? { pexp_desc = Pexp_constant (Pconst_string _); _ } as s]
       [%e? { pexp_desc = Pexp_constant (Pconst_float _); _ } as f]] ->
-    [], [%expr Network.return_term (Operation.number ~axis_label:[%e s] [%e f])]
+    no_vbs, [%expr Network.return_term (Operation.number ~axis_label:[%e s] [%e f])]
 
   | [%expr [%e? { pexp_desc = Pexp_constant (Pconst_string _); _ } as s]
       [%e? { pexp_desc = Pexp_constant (Pconst_integer _); _ } as i]] ->
-    [], [%expr Network.return_term (Operation.number ~axis_label:[%e s] (Float.of_int [%e i]))]
+    no_vbs, [%expr Network.return_term (Operation.number ~axis_label:[%e s] (Float.of_int [%e i]))]
 
   | { pexp_desc = Pexp_constant (Pconst_string (ident, str_loc, _)); _ } ->
     let pat = Ast_helper.Pat.var ~loc {loc=str_loc; txt=ident} in
     let v = [%expr Network.return_term Operation.O.(!~ [%e expr])] in
     let vb = Ast_helper.Vb.mk ~loc pat v in
-    [vb], pat2expr pat
+    Map.singleton (module String) ident vb, pat2expr pat
 
   | { pexp_desc = Pexp_tuple _; _ } | { pexp_desc = Pexp_array _; _ } 
   | { pexp_desc = Pexp_construct ({txt=Lident "::"; _}, _); _ } ->
-    [], ndarray_constant expr
+    no_vbs, ndarray_constant expr
     
   | [%expr [%e? expr1] [%e? expr2] [%e? expr3] ] ->
     let vbs1, e1 = translate expr1 in
     let vbs2, e2 = translate expr2 in
     let vbs3, e3 = translate expr3 in
-    vbs1 @ vbs2 @ vbs3, [%expr Network.apply (Network.apply [%e e1] [%e e2]) [%e e3]]
+    reduce_vbss [vbs1; vbs2; vbs3],
+    [%expr Network.apply (Network.apply [%e e1] [%e e2]) [%e e3]]
 
   | [%expr [%e? expr1] [%e? expr2] ] ->
     let vbs1, e1 = translate expr1 in
     let vbs2, e2 = translate expr2 in
-    vbs1 @ vbs2, [%expr Network.apply [%e e1] [%e e2]]
+    Map.merge_skewed vbs1 vbs2 ~combine:(fun ~key:_ _v1 v2 -> v2),
+    [%expr Network.apply [%e e1] [%e e2]]
 
   | [%expr fun ~config [%p? pat1] [%p? pat2] -> [%e? body] ] ->
     (* TODO(#38): generalize config to any number of labeled arguments with any labels. *)
@@ -173,7 +178,7 @@ let rec translate expr =
     let pat2_ref = pat2pat_ref pat2 in
     let vbs, body = translate body in
     let body = let_opt ~loc vbs body in
-    [], [%expr
+    no_vbs, [%expr
       fun ~config ->
         let [%p pat1_ref] = ref [] in
         let [%p pat1] = Network.return (Network.Placeholder [%e pat2expr @@ pat1_ref]) in
@@ -194,7 +199,7 @@ let rec translate expr =
     let pat_ref = pat2pat_ref pat in
     let vbs, body = translate body in
     let body = let_opt ~loc vbs body in
-    [], [%expr
+    no_vbs, [%expr
       fun ~config ->
         let [%p pat_ref] = ref [] in
         let [%p pat] = Network.return (Network.Placeholder [%e pat2expr @@ pat_ref]) in
@@ -254,12 +259,12 @@ let rec translate expr =
   | [%expr [%e? expr1] ; [%e? expr2] ] ->
     let vbs1, e1 = translate expr1 in
     let vbs2, e2 = translate expr2 in
-    vbs1 @ vbs2, [%expr [%e e1] ; [%e e2]]
+    Map.merge_skewed vbs1 vbs2 ~combine:(fun ~key:_ _v1 v2 -> v2), [%expr [%e e1] ; [%e e2]]
 
   | [%expr if [%e? expr1] then [%e? expr2] else [%e? expr3]] ->
     let vbs2, e2 = translate expr2 in
     let vbs3, e3 = translate expr3 in
-    vbs2 @ vbs3, [%expr if [%e expr1] then [%e e2] else [%e e3]]
+    Map.merge_skewed vbs2 vbs3 ~combine:(fun ~key:_ _v1 v2 -> v2), [%expr if [%e expr1] then [%e e2] else [%e e3]]
 
   | [%expr if [%e? expr1] then [%e? expr2]] ->
     let vbs2, e2 = translate expr2 in
@@ -271,7 +276,7 @@ let rec translate expr =
          ~f:(fun ({pc_rhs; _} as c) ->
             let vbs, pc_rhs = translate pc_rhs in
             vbs, {c with pc_rhs}) in
-     List.concat vbss, { expr with pexp_desc = Pexp_match (expr1, cases) }
+     reduce_vbss vbss, { expr with pexp_desc = Pexp_match (expr1, cases) }
 
   | { pexp_desc = Pexp_let (recflag, bindings, body); _ } ->
      let vbss1, bindings = List.unzip @@ List.map bindings
@@ -279,8 +284,8 @@ let rec translate expr =
           let vbs, pvb_expr = translate binding.pvb_expr in
           vbs, {binding with pvb_expr}) in
      let vbs2, body = translate body in
-     let all_bindings = List.concat (vbss1 @ [bindings; vbs2]) in
-     [], {expr with pexp_desc=Pexp_let (recflag, all_bindings, body)}
+     let all_bindings = (Map.data @@ reduce_vbss vbss1) @ bindings @ Map.data vbs2 in
+     no_vbs, {expr with pexp_desc=Pexp_let (recflag, all_bindings, body)}
 
   | { pexp_desc = Pexp_open (decl, body); _ } ->
     let vbs, body = translate body in
@@ -291,7 +296,7 @@ let rec translate expr =
      vbs, {expr with pexp_desc=Pexp_letmodule (name, module_expr, body)}
 
   | expr ->
-    [], expr
+    no_vbs, expr
 
 let expr_expander ~loc ~path:_ payload =
   match payload with
@@ -302,7 +307,7 @@ let expr_expander ~loc ~path:_ payload =
         let vbs, v = translate vb.pvb_expr in
         vbs, {vb with pvb_expr=[%expr let open! Network.O in [%e v]]}) in
      let expr = {payload with pexp_desc=Pexp_let (recflag, bindings, body)} in
-     let_opt ~loc (List.concat vbss) expr
+     let_opt ~loc (reduce_vbss vbss) expr
   | expr ->
     let vbs, expr = translate expr in
     let_opt ~loc vbs expr
