fed0c3a3e9f170b9d74c34d2b79aa381cd55d7c4
lukstafi <lukstafi@gmail.com>
2023-03-17 15:15:14 +0100

    In progress: non-form-level syntax `%nn_cd` It will be used to implement complex `Code.t` computations (for value / forward and gradient / backprop specifications).

--- lib/ppx_ocannl.ml
+++ lib/ppx_ocannl.ml
@@ -4,6 +4,10 @@ open Ppxlib
 
 let rules =
   [Ppxlib.Context_free.Rule.extension  @@
+  Extension.declare "nn_cd" Extension.Context.expression Ast_pattern.(single_expr_payload __) Ppx_nn_cd.expr_expander;
+  Ppxlib.Context_free.Rule.extension  @@
+  Extension.declare "nn_cd" Extension.Context.structure_item Ast_pattern.(pstr __) Ppx_nn_cd.str_expander;
+  Ppxlib.Context_free.Rule.extension  @@
    Extension.declare "nn_op" Extension.Context.expression Ast_pattern.(single_expr_payload __) Ppx_nn_op.expr_expander;
    Ppxlib.Context_free.Rule.extension  @@
    Extension.declare "nn_op" Extension.Context.structure_item Ast_pattern.(pstr __) Ppx_nn_op.str_expander;
