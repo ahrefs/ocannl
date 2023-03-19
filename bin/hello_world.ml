4f2780892fb3ee2f41b104fa36a5d251cffca569
lukstafi <lukstafi@gmail.com>
2023-03-19 15:45:28 +0100

    Fix matrix `*` in tests + promote expectations

--- bin/hello_world.ml
+++ bin/hello_world.ml
@@ -2,12 +2,13 @@ open Base
 open Ocannl
 module DSL = Operation.DSL
 
-let() =
+let () = Session.DSL.set_executor `OCaml
+
+let hello1() =
   Session.drop_session();
   Random.init 0;
   let open Operation.DSL in
   let open Session.DSL in
-  set_executor `OCaml;
   (* Hey is inferred to be a matrix. *)
   let hey =
     range_of_shape ~batch_dims:[7] ~input_dims:[9; 10; 11] ~output_dims:[13; 14] () in
@@ -16,3 +17,35 @@ let() =
   print_node_tree ~with_grad:false ~depth:99 hoo.node_id;
   print_formula ~with_code:false ~with_grad:false `Default hoo
   (* Disable line wrapping for viewing the output. In VSCode: `View: Toggle Word Wrap`. *)
+
+let hello2() =
+  let open Session.DSL in
+  drop_session();
+  Random.init 0;
+  (* Hey is inferred to be a matrix. *)
+  let%nn_op y = "hey" * 'q' 2.0 + 'p' 1.0 in
+  (* Punning for ["hey"] above introduced the [hey] identifier. *)
+  refresh_session ();
+  print_preamble();
+  print_formula ~with_code:false ~with_grad:false `Default @@ hey;
+  print_formula ~with_code:false ~with_grad:false `Default @@ y
+
+let hello3() =
+  let open Session.DSL in
+  drop_session();
+  Random.init 0;
+  (* Hey is inferred to be a matrix. *)
+  let hey = DSL.O.(!~ "hey") in
+  let zero_to_twenty = DSL.range 20 in
+  let _y = DSL.O.(hey * zero_to_twenty + zero_to_twenty) in
+  refresh_session ();
+  print_preamble();
+  print_session_code();
+  Caml.Format.print_newline();
+  print_formula ~with_code:true ~with_grad:false `Inline zero_to_twenty;
+  Caml.Format.print_newline();
+  print_formula ~with_code:true ~with_grad:false `Default zero_to_twenty;
+  Caml.Format.print_newline();
+  print_formula ~with_code:true ~with_grad:false `Default hey
+
+let () = ignore (hello1, hello2); hello3()
