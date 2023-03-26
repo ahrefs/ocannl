3d4bc0af1b298fdef45e5c23eb8961be25ff1e76
lukstafi <lukstafi@gmail.com>
2023-03-26 22:48:04 +0200

    Micrograd demo half-moons synthetic dataset

--- test/micrograd_demo.ml
+++ test/micrograd_demo.ml
@@ -76,5 +76,68 @@ let%expect_test "Micrograd half-moons example" =
   let open Session.SDSL in
   drop_session();
   Random.init 0;
-  (* FIXME: in progress *)
-  ()
+  let len = 100 in
+  let batch = 10 in
+  let noise() = Random.float_range (-0.1) 0.1 in
+  let moons_flat = Array.concat_mapi (Array.create ~len ()) ~f:Float.(fun i () ->
+    let v = of_int i * pi / of_int len in
+    let c = cos v and s = sin v in
+    [|c + noise(); s + noise(); 1.0 - c + noise(); 0.5 - s + noise()|]) in
+  let moons_classes = Array.init (len*2) ~f:(fun i -> if i % 2 = 0 then 1. else (-1.)) in
+  let moons_input = FDSL.data ~label:"moons_input" ~batch_dims:[batch] ~output_dims:[2]
+      (Init_op (Fixed_constant moons_flat)) in
+  let moons_class = FDSL.data ~label:"moons_class" ~batch_dims:[batch] ~output_dims:[1]
+      (Init_op (Fixed_constant moons_classes)) in
+  let module A = Bigarray.Genarray in
+  let points1 = ref [] in
+  let points2 = ref [] in
+  for _step = 1 to 2 * len/batch do
+    refresh_session ();
+    let points = NodeUI.retrieve_2d_points ~xdim:0 ~ydim:1 moons_input.node.node.value in
+    let classes = NodeUI.retrieve_1d_points ~xdim:0 moons_class.node.node.value in
+    let npoints1, npoints2 = Array.partitioni_tf points ~f:Float.(fun i _ -> classes.(i) > 0.) in
+    points1 := npoints1 :: !points1;
+    points2 := npoints2 :: !points2;
+  done;
+  let canvas = 
+    let open PrintBox_utils in
+    plot ~size:(75, 35)
+      [Scatterplot {points=Array.concat !points1; pixel="#"}; 
+       Scatterplot {points=Array.concat !points2; pixel="%"}] in
+  PrintBox_text.output Stdio.stdout @@ PrintBox.grid_text ~bars:false canvas;
+  [%expect {|
+                                #
+                        #
+                     ##   ##   #      #
+                  #   #     #  #
+                ##     #   #     # ##  # #
+              #   ##    ##          #   # #   #
+           #  #  #                   # #  #
+             #  #                      #   #   #
+            #                              ## #
+         #   #
+        #  #                                 #
+          ##                                  ##
+       #                   %                   # #
+                                               #  #                           %
+      # ##                 %  %                                          %
+     #                      %%                  #                            %
+     ##                      %%%                  ###                     %
+    # #                     %                        #                    %
+       #                                         #  #                   %%%%%
+     #                         %                     #                 %% %
+       #                    %                      ## #
+       #                       %                                        % %
+    ##                       %% %%                   #                %
+                               %   %               #                   %%
+                              %    %                                 %%
+                                    %                              %     %
+                                     %%                              %   %
+                                   %  %                           %  %
+                                  %
+                                         %                     %  %%
+                                      %  % % %              %  %
+                                       %%   % %  %  %%  %      %   %
+                                       %  %%      %    % % %%  %
+                                           %        % % %    %
+                                                 %%    %  % |}]
