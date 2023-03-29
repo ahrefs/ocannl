e98e35f977fae51c5191c2c897b06a70c4ca77e8
lukstafi <lukstafi@gmail.com>
2023-03-29 23:35:11 +0200

    Preparing test(s) for `einsum`

---
+++ bin/einsum_trivia.ml
@@ -0,0 +1,17 @@
+open Base
+open Ocannl
+
+module FDSL = Operation.FDSL
+
+let () = Session.SDSL.set_executor OCaml
+
+let () =
+  let open Session.SDSL in
+  drop_session();
+  Random.init 0;
+  let hey =
+    FDSL.range_of_shape ~batch_dims:[2] ~input_dims:[3] ~output_dims:[4] () in
+  let%nn_op ho = hey++"b|i->o => o|b->i" in
+  refresh_session ();
+  print_formula ~with_code:false ~with_grad:false `Default @@ hey;
+  print_formula ~with_code:false ~with_grad:false `Default @@ ho
