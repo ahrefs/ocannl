e92a9ee940f2cf7c4cc01195bc99c58bd4ffbd3d
lukstafi <lukstafi@gmail.com>
2023-03-31 12:47:47 +0200

    More broadcast, sum out tests

--- bin/einsum_trivia.ml
+++ bin/einsum_trivia.ml
@@ -10,10 +10,10 @@ let () =
   drop_all_sessions();
   Random.init 0;
   let a =
-    FDSL.range_of_shape ~batch_dims:[] ~input_dims:[] ~output_dims:[2] () in
+    FDSL.range_of_shape ~batch_dims:[3] ~input_dims:[4] ~output_dims:[2] () in
   let b =
-    FDSL.range_of_shape ~batch_dims:[] ~input_dims:[] ~output_dims:[3] () in
-  let%nn_op c = (a + 1) *+"i; j => i->j" b in
+    FDSL.range_of_shape ~batch_dims:[3] ~input_dims:[1] ~output_dims:[4] () in
+  let%nn_op c = a *+"...|i->...; ...|...->i => ...|i" b in
   refresh_session ();
   print_formula ~with_code:false ~with_grad:false `Default @@ a;
   print_formula ~with_code:false ~with_grad:false `Default @@ b;
