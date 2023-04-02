3a5783e0036bdf99b76db63f23e0159d9de97be4
lukstafi <lukstafi@gmail.com>
2023-04-02 10:05:31 +0200

    Tests for einsum fixed dim axes

--- bin/einsum_trivia.ml
+++ bin/einsum_trivia.ml
@@ -13,7 +13,7 @@ let () =
     FDSL.range_of_shape ~batch_dims:[3] ~input_dims:[4] ~output_dims:[2] () in
   let b =
     FDSL.range_of_shape ~batch_dims:[3] ~input_dims:[1] ~output_dims:[4] () in
-  let%nn_op c = a *+"...|i->...; ...|...->i => ...|i" b in
+  let%nn_op c = a *+"...|i->1; ...|...->i => ...|i" b in
   refresh_session ();
   print_formula ~with_code:false ~with_grad:false `Default @@ a;
   print_formula ~with_code:false ~with_grad:false `Default @@ b;
