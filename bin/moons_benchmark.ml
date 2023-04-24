0429be9503d66406514a1f90940a5aaa38a17b0e
lukstafi <lukstafi@gmail.com>
2023-04-24 23:40:44 +0200

    Improved benchmark

--- bin/moons_benchmark.ml
+++ bin/moons_benchmark.ml
@@ -17,8 +17,8 @@ let classify_moons executor () =
   Random.init 0;
   (* let hid1 = 64 in *)
   let len = 400 in
-  let batch = 100 in
-  let epochs = 1000 in
+  let batch = 10 in
+  let epochs = 10000 in
   let steps = epochs * 2 * len / batch in
   let noise () = Random.float_range (-0.1) 0.1 in
   let moons_flat =
@@ -33,14 +33,24 @@ let classify_moons executor () =
   in
   let moons_flat =
     FDSL.term ~label:"moons_flat" ~batch_dims:[ epochs; batch ] ~input_dims:[] ~output_dims:[ 2 ]
-    @@ First (Constant_fill moons_flat)
+      ~init_op:(Constant_fill moons_flat) ()
   in
   let moons_classes = Array.init (len * 2) ~f:(fun i -> if i % 2 = 0 then 1. else -1.) in
   let moons_classes =
-    FDSL.term ~label:"moons_flat" ~batch_dims:[ epochs; batch ] ~input_dims:[] ~output_dims:[ 1 ]
-    @@ First (Constant_fill moons_classes)
+    FDSL.term ~label:"moons_classes" ~batch_dims:[ epochs; batch ] ~input_dims:[] ~output_dims:[ 1 ]
+      ~init_op:(Constant_fill moons_classes) ()
   in
-  let%nn_op mlp x = "b4" 1 + ("w4" * !/("b3" 16 + ("w3" * !/("b2" 32 + ("w2" * !/("b1" 64 + ("w1" * x))))))) in
+  let%nn_op mlp x =
+    "b6" 1
+    + "w6"
+      * !/("b4" 4
+          + "w4"
+            * !/("b2" 8
+                + ("w2" * !/("b1" 16 + ("w1" * x)))
+                + "b3" 8
+                + ("w3" * !/(b2 + (w2 * !/(b1 + (w1 * x))))))
+          + ("b5" 4 + ("w5" * !/(b4 + (w4 * !/(b3 + (w3 * !/(b2 + (w2 * !/(b1 + (w1 * x)))))))))))
+  in
   let session_step =
     FDSL.data ~label:"session_step" ~batch_dims:[] ~output_dims:[ 1 ] (fun ~n -> Synthetic [%nn_cd n =+ 1])
   in
@@ -57,29 +67,33 @@ let classify_moons executor () =
   let learning_rates = ref [] in
   let%nn_op margin_loss = !/(1 - (moons_class *. mlp moons_input)) in
   let%nn_op ssq w = (w **. 2) ++ "...|...->... => 0" in
-  let reg_loss = List.map ~f:ssq [ w1; w2; w3; b1; b2; b3 ] |> List.reduce_exn ~f:FDSL.O.( + ) in
-  let%nn_op total_loss = ((margin_loss ++ "...|... => 0") /. !..batch) + (0.0001 *. reg_loss) in
+  let reg_loss =
+    List.map ~f:ssq [ w1; w2; w3; w4; w5; w6; b1; b2; b3; b4; b5; b6 ] |> List.reduce_exn ~f:FDSL.O.( + )
+  in
+  let%nn_op total_loss = ((margin_loss ++ "...|... => 0") /. !..batch) + (0.001 *. reg_loss) in
   for step = 1 to steps do
     refresh_session ();
     Option.value_exn !update_params ();
-    let points = NodeUI.retrieve_2d_points ~xdim:0 ~ydim:1 moons_input.node.node.value in
-    let classes = NodeUI.retrieve_1d_points ~xdim:0 moons_class.node.node.value in
-    let npoints1, npoints2 = Array.partitioni_tf points ~f:Float.(fun i _ -> classes.(i) > 0.) in
-    points1 := npoints1 :: !points1;
-    points2 := npoints2 :: !points2;
-    let mlr = NodeUI.retrieve_1d_points ~xdim:0 (Option.value_exn !minus_learning_rate).node.node.value in
-    assert (Array.length mlr = 1);
-    learning_rates := ~-.(mlr.(0)) :: !learning_rates;
-    let batch_loss = NodeUI.retrieve_1d_points ~xdim:0 total_loss.node.node.value in
-    assert (Array.length batch_loss = 1);
-    losses := batch_loss.(0) :: !losses;
-    log_losses := Float.log batch_loss.(0) :: !log_losses;
-    if step % 10000 = 0 then (
-      Stdio.printf "Minus learning rate over batch for step %d: %f\n%!" step mlr.(0);
-      Stdio.printf "Loss over batch for step %d: %f\n%!" step batch_loss.(0);
-      let step_no = NodeUI.retrieve_1d_points ~xdim:0 session_step.node.node.value in
-      assert (Array.length step_no = 1);
-      Stdio.printf "Step index at step %d: %f\n%!" step step_no.(0))
+    if step <= len then (
+      let points = NodeUI.retrieve_2d_points ~xdim:0 ~ydim:1 moons_input.node.node.value in
+      let classes = NodeUI.retrieve_1d_points ~xdim:0 moons_class.node.node.value in
+      let npoints1, npoints2 = Array.partitioni_tf points ~f:Float.(fun i _ -> classes.(i) > 0.) in
+      points1 := npoints1 :: !points1;
+      points2 := npoints2 :: !points2);
+    if step % 1000 = 0 then (
+      let mlr = NodeUI.retrieve_1d_points ~xdim:0 (Option.value_exn !minus_learning_rate).node.node.value in
+      assert (Array.length mlr = 1);
+      learning_rates := ~-.(mlr.(0)) :: !learning_rates;
+      let batch_loss = NodeUI.retrieve_1d_points ~xdim:0 total_loss.node.node.value in
+      assert (Array.length batch_loss = 1);
+      losses := batch_loss.(0) :: !losses;
+      log_losses := Float.log batch_loss.(0) :: !log_losses;
+      if step % 50000 = 0 then (
+        Stdio.printf "Minus learning rate over batch for step %d: %f\n%!" step mlr.(0);
+        Stdio.printf "Loss over batch for step %d: %f\n%!" step batch_loss.(0);
+        let step_no = NodeUI.retrieve_1d_points ~xdim:0 session_step.node.node.value in
+        assert (Array.length step_no = 1);
+        Stdio.printf "Step index at step %d: %f\n%!" step step_no.(0)))
   done;
   close_session ();
   let%nn_op point = [ 0; 0 ] in
@@ -139,3 +153,14 @@ let _suspended () = classify_moons Gccjit ()
 let () =
   List.map benchmarks ~f:(fun (name, test) -> Bench.Test.create ~name test)
   |> Bench.make_command |> Command_unix.run
+
+(* Example output, before the single-use-not-memorized aka. virtual nodes optimization:
+
+   ┌─────────────┬───────────┬────────────────┬──────────────┬──────────────┬────────────┐
+   │ Name        │  Time/Run │        mWd/Run │     mjWd/Run │     Prom/Run │ Percentage │
+   ├─────────────┼───────────┼────────────────┼──────────────┼──────────────┼────────────┤
+   │ Interpreter │ 3_705.02s │ 1_316_342.55Mw │ 178_312.43kw │ 178_176.42kw │    100.00% │
+   │ OCaml       │ 1_405.22s │   384_153.02Mw │  14_864.72kw │  14_658.17kw │     37.93% │
+   │ gccjit      │    10.76s │       286.06Mw │     737.77kw │     703.35kw │      0.29% │
+   └─────────────┴───────────┴────────────────┴──────────────┴──────────────┴────────────┘
+*)
