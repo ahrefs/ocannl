f8a8f8e05b427e69be00cf2d5c38e05474d90d94
lukstafi <lukstafi@gmail.com>
2023-08-30 17:09:44 +0200

    Merge `Backend` and `Devices` into `Backends`

--- arrayjit/lib/backends.ml
+++ arrayjit/lib/backends.ml
@@ -121,3 +121,24 @@ module Multicore_backend (Backend : No_device_backend) : Backend = struct
   let get_ctx_device { device; _ } = device
   let to_ordinal device = device.ordinal
 end
+
+module Gccjit_device : No_device_backend with type context = Exec_as_gccjit.context = struct
+  type context = Exec_as_gccjit.context
+  type compiled = Exec_as_gccjit.compiled = { context : context; run : unit -> unit }
+
+  open Exec_as_gccjit
+
+  let initialize () = ()
+  let unsafe_cleanup () = ()
+  let init = init
+  let finalize = finalize
+
+  let compile context ~name ?verbose code =
+    jit context ~name ?verbose @@ Assignments.compile_proc ~name ?verbose code
+
+  let from_host = from_host
+  let to_host = to_host
+  let merge = merge
+end
+
+module Gccjit_backend = Multicore_backend (Gccjit_device)
