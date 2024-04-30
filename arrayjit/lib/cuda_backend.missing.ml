b625f78dd0b140e4b9b538811dfff6300b5d59d5
Lukasz Stafiniak <lukstafi@gmail.com>
2024-04-30 20:29:20 +0200

    Fix last commit (cuda_backend.missing.ml)

--- arrayjit/lib/cuda_backend.missing.ml
+++ arrayjit/lib/cuda_backend.missing.ml
@@ -6,13 +6,12 @@ type code = Indexing.unit_bindings [@@deriving sexp_of]
 let initialize () = ()
 let is_initialized () = true
 let finalize _context = ()
-
 let compile ?name:_ bindings _optimized = bindings
 
-let link context code =
+let link (Unimplemented : context) code =
   let compiled_bindings = List.map ~f:(fun s -> (s, ref 0)) @@ Indexing.bound_symbols code in
   let work () = Tnode.Work (fun _debug_runtime () -> ()) in
-  (context, compiled_bindings, work)
+  ((Unimplemented : context), compiled_bindings, work)
 
 let unsafe_cleanup ?unsafe_shutdown:_ () = ()
 let from_host _context _arr = false
@@ -21,9 +20,9 @@ let merge ?name_suffix:_ _arr ~dst:_ ~accum:_ ~src:_ _bindings = None
 
 type device = Unimplemented [@@deriving sexp_of]
 
-let init _device = failwith "CUDA missing: install cudajit"
+let init (Unimplemented : device) : context = Unimplemented
 let await _device = ()
-let num_devices () = 1
+let num_devices () = 0
 let get_device ~ordinal:_ = failwith "CUDA missing: install cudajit"
-let get_ctx_device _context = failwith "CUDA missing: install cudajit"
-let to_ordinal _device = 0
+let get_ctx_device (Unimplemented : context) : device = Unimplemented
+let to_ordinal (Unimplemented : device) : int = failwith "CUDA missing: install cudajit"
