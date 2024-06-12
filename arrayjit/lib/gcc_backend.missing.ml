77f03ede4ea021d1793d62b2476fa0795def4919
Lukasz Stafiniak <lukstafi@gmail.com>
2024-06-12 16:23:48 +0200

    Fix gccjit mock

--- arrayjit/lib/gcc_backend.missing.ml
+++ arrayjit/lib/gcc_backend.missing.ml
@@ -1,11 +1,11 @@
 type context = Unimplemented_ctx [@@deriving sexp_of]
 type procedure = Unimplemented_proc [@@deriving sexp_of]
-type ctx_arrays = Ndarray.t Map.M(Tnode).t [@@deriving sexp_of]
+type ctx_arrays = Ndarray.t Base.Map.M(Tnode).t [@@deriving sexp_of]
 
 type config = [ `Physical_devices_only | `For_parallel_copying | `Most_parallel_devices ]
 [@@deriving equal, sexp, variants]
 
-let ctx_arrays Unimplemented_ctx = Map.empty (module Tnode)
+let ctx_arrays Unimplemented_ctx = Base.Map.empty (module Tnode)
 let compile ~name:_ ~opt_ctx_arrays:_ _bindings _code = Unimplemented_proc
 
 let compile_batch ~names:_ ~opt_ctx_arrays:_ _bindings _codes =
@@ -24,7 +24,7 @@ let device_to_device ?rt:_ _tn ~into_merge_buffer:_ ~dst:_ ~src:_ =
   failwith "backend missing: install the corresponding optional dependency"
 
 let physical_merge_buffers = false
-let name = "MISSING_SIMPLE_BACKEND"
+let name = "gccjit"
 let initialize () = ()
 let is_initialized () = true
 let init ~label:_ = Unimplemented_ctx
