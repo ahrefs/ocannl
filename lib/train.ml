8bc92b356b4f1f05e3d7f4313b0eb7305ac22a28
Lukasz Stafiniak <lukstafi@gmail.com>
2024-07-01 21:42:35 +0200

    Tiny fix: `merge_grads` should copy grad into merge_buffer

--- lib/train.ml
+++ lib/train.ml
@@ -339,7 +339,8 @@ let%track_sexp parallel_update (type context) (module Backend : Backend_type wit
     Array.iteri all_params ~f:(fun i p ->
         let grad_merge = Option.value_exn grad_merges_to.(to_).(i) in
         assert (
-          Backend.device_to_device p.value ~into_merge_buffer:Copy ~dst:grad_merge.context ~src:ctxs.(from));
+          Backend.device_to_device (Option.value_exn p.diff).grad ~into_merge_buffer:Copy
+            ~dst:grad_merge.context ~src:ctxs.(from));
         (Tn.run debug_rt grad_merge.schedule : unit))
   in
   let merge_loss ~src =
