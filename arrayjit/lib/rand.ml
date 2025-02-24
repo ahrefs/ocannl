718613f5811d64e8c8268b5c6d707571ff304781
Lukasz Stafiniak <lukstafi@gmail.com>
2025-02-24 17:49:15 +0100

    Temporary Experiment 5

--- arrayjit/lib/rand.ml
+++ arrayjit/lib/rand.ml
@@ -27,15 +27,35 @@ module Random_for_tests = struct
   let int high = Int32.(to_int_trunc @@ (rand_int32 () % of_int_trunc high))
 end
 
+module Random_for_dummy_tests = struct
+  let rand = ref (1l : Int32.t)
+
+  let rand_int32 () =
+    let open Int32 in
+    rand := !rand + 1l;
+    if equal !rand 10000l then rand := 1l;
+    !rand
+
+  let init seed = rand := Int32.(of_int_trunc seed + 1l)
+
+  let float_range low high =
+    let raw = Int32.(to_float @@ rand_int32 ()) in
+    (raw /. 10000. *. (high -. low)) +. low
+
+  let char () = Char.of_int_exn @@ Int32.(to_int_trunc @@ (rand_int32 () % 256l))
+  let int high = Int32.(to_int_trunc @@ (rand_int32 () % of_int_trunc high))
+end
+
 let random_config = Utils.get_global_arg ~arg_name:"randomness_lib" ~default:"stdlib"
 
 let random_lib =
   match random_config with
   | "stdlib" -> (module Base.Random : Random)
   | "for_tests" -> (module Random_for_tests : Random)
+  | "for_dummy_tests" -> (module Random_for_dummy_tests : Random)
   | _ ->
       invalid_arg
       @@ "Rand.random_lib: invalid setting of the global argument randomness_lib, expected one of: \
-          stdlib, for_tests; found: " ^ random_config
+          stdlib, for_tests, for_dummy_tests; found: " ^ random_config
 
 module Lib = (val random_lib)
