7254cbab51c86ded6c6b6fe0a863ec43877fbb84
Lukasz Stafiniak <lukstafi@gmail.com>
2025-08-31 16:49:20 +0200

    Yay! Fixed the unnecessary dune rules triggering! Also, formatting

--- test/operations/test_index_prec.ml
+++ test/operations/test_index_prec.ml
@@ -5,19 +5,17 @@ module Utils = Utils
 let () =
   (* Test with default big_models=false, should use uint32 *)
   let idx_prec = Ops.index_prec () in
-  Stdio.printf "With big_models=false, index precision: %s\n" 
-    (Ops.prec_string idx_prec);
+  Stdio.printf "With big_models=false, index precision: %s\n" (Ops.prec_string idx_prec);
   assert (String.equal (Ops.prec_string idx_prec) "uint32");
-  
+
   (* Test with big_models=true, should use uint64 *)
   let old_setting = Utils.settings.big_models in
   Utils.settings.big_models <- true;
   let idx_prec = Ops.index_prec () in
-  Stdio.printf "With big_models=true, index precision: %s\n" 
-    (Ops.prec_string idx_prec);
+  Stdio.printf "With big_models=true, index precision: %s\n" (Ops.prec_string idx_prec);
   assert (String.equal (Ops.prec_string idx_prec) "uint64");
-  
+
   (* Restore the old setting *)
   Utils.settings.big_models <- old_setting;
-  
+
   Stdio.printf "Index precision tests passed!\n"
