d6121e6e9da50d866737f9842e3140743e722d0a
Lukasz Stafiniak <lukstafi@gmail.com>
2025-11-28 12:31:44 +0100

    Update hidden dims heuristic test

--- test/operations/test_param_shape_error.ml
+++ test/operations/test_param_shape_error.ml
@@ -13,8 +13,8 @@ let default_lone_param () =
     Stdio.print_endline "\nERROR: Should have raised an exception"
   with Row.Shape_error (msg, _) -> Stdio.printf "Got acceptable error: %s\n" msg
 
-let default_param_1d () =
-  Stdio.printf "Testing default parameter 1D -- corner case\n";
+let lone_param_1d () =
+  Stdio.printf "Testing lone parameter 1D -- corner case\n";
   Tensor.unsafe_reinitialize ();
   (* This should raise an error because we have a parameter with unspecified dimensions *)
   try
@@ -57,9 +57,33 @@ let default_affine_op_unknown_input () =
     ()
   with Row.Shape_error (msg, _) -> Stdio.printf "Got expected error: %s\n" msg
 
+let default_bias_param () =
+  Stdio.printf "Testing default bias parameter\n";
+  Tensor.unsafe_reinitialize ();
+  (* This should raise an error because we have a parameter with unspecified dimensions *)
+  try
+    let%op w_o = { x } + { y } in
+    let _ctx : Context.t = Train.init_params (Context.auto ()) Train.IDX.empty w_o in
+    Train.printf w_o;
+    Stdio.print_endline "\nERROR: Should have raised an exception"
+  with Row.Shape_error (msg, _) -> Stdio.printf "Got expected error: %s\n" msg
+
+let default_bias_param_1d () =
+  Stdio.printf "Testing bias parameter 1D\n";
+  Tensor.unsafe_reinitialize ();
+  (* This should raise an error because we have a parameter with unspecified dimensions *)
+  try
+    let%op w_o = { x = uniform1 () } + { y = uniform1 () } in
+    let _ctx : Context.t = Train.init_params (Context.auto ()) Train.IDX.empty w_o in
+    Train.printf w_o;
+    ()
+  with Row.Shape_error (msg, _) -> Stdio.printf "Got expected error: %s\n" msg
+
 let () =
-  ignore default_lone_param;
-  default_param_1d ();
+  default_lone_param ();
+  lone_param_1d ();
   default_linear_op ();
   default_affine_op_propagated ();
-  default_affine_op_unknown_input ()
+  default_affine_op_unknown_input ();
+  default_bias_param ();
+  default_bias_param_1d ()
